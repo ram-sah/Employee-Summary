f1780e47ee1ebf878d9e5d77b4d0e723384645fd
Ram Sah <mohan2036@gmail.com>
2020-07-29 18:26:15 -0400

    added code on intern.js and tested, worked

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,13 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        //name, id & email has already initialised in Employee.js
+        super(name, id, email);
+        this.school = school;
+    }
+    getSchool() {
+        return this.school;
+    }
+}
+module.exports = Intern;

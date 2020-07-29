3f5c6358978cba88b9ddad1bdad3749babb4db86
Ram Sah <mohan2036@gmail.com>
2020-07-29 17:08:15 -0400

    modified readme file

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    //Below Code will remove the redundancy of adding a getRole function to the other child classes
+    getRole() {
+        return this.constructor.name;
+    }
+}
+
+module.exports = Employee;
